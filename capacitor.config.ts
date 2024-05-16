55e1d3af321a8504726ec7364d264f12a04f099d
Fernando Barrios <fer.b.1990@gmail.com>
2024-05-16 00:52:34 -0600

    feat: update capacitor for android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'udv-ia-curso',
+  appId: 'com.jfbarrios.udviacurso',
+  appName: 'App',
   webDir: 'dist'
 };
 
